7e272517606762b275094f520811b2ac6fbfa854
UserHyuser <drachevskii@yandex.ru>
2019-10-06 18:00:10 +0700

    some func added

--- lodash-rewrite.js
+++ lodash-rewrite.js
@@ -1,4 +1,5 @@
-// chunk Array
+const _ = require('lodash');
+
 function chunkArray(arr, size) {
     let newArray = [];
     for (let i = 0; i < arr.length; i++){
@@ -12,4 +13,37 @@ function chunkArray(arr, size) {
     }
     return newArray;
 }
-console.log(chunkArray(['a', 'b', 'c', 'd','e','f','g',"w",'t'], 5))
+// console.log(chunkArray(['a', 'b', 'c', 'd','e','f','g',"w",'t'], 5));
+
+function concatArray() {
+    var result = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+        if (Array.isArray(arguments[i])){
+            let tmp = arguments[i];
+            for (let j = 0; j< arguments[i].length; j++){
+                result.push(tmp[j])
+            }
+            continue
+        }
+        result.push(arguments[i]);
+    }
+    return result;
+}
+/*
+console.log(concatArray([2,4,5,6,7], 5, [6,7,8,9], [[8]]));
+console.log(_.concat([2,4,5,6,7], 5, [6,7,8,9], [[8]]));*/
+
+// return difference between arrays (check only elements from Arr1 in Arr2 !like in the lodash lib)
+function difference(arr1, arr2) {
+    let diffArray = [];
+    for (let i = 0; i < arr1.length; i++){
+        if(arr2.indexOf(arr1[i])){
+            diffArray.push(arr1[i])
+        }
+    }
+    return diffArray;
+}
+/*console.log(difference([2, 1], [2, 3, 5]));
+console.log(_.difference([2, 1], [2, 3, 5]));*/
+
+
