99692a5bfd716b24a9c469fd285db265a79c7a28
UserHyuser <drachevskii@yandex.ru>
2019-10-10 18:17:57 +0700

    new func

--- codewars.js
+++ codewars.js
@@ -185,4 +185,17 @@ function getW(height) {
     }
     else return [];
 }
-console.log(getW(2))
+// console.log(getW(2))
+
+/*I assume most of you are familiar with the ancient legend of the rice (but I see wikipedia suggests wheat, for some reason) problem,
+but a quick recap for you: a young man asks as a compensation only 1 grain of rice for the first square, 2 grains for the second, 4 for the third,
+8 for the fourth and so on, always doubling the previous.
+
+Your task is pretty straightforward (but not necessarily easy): given an amount of grains, you need to return up to which square of the chessboard one
+should count in order to get at least as many.
+* */
+function squaresNeeded(grains){
+    let deg = 0;
+    while (grains > 0) { grains = grains - 2 ** (deg); deg++; }
+    return deg;
+}
